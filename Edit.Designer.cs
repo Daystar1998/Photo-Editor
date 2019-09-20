6d7c68304dbf26bf9f9554aca6aad80962d6ee6a
Matthew Day <Daystar.1998@yahoo.com>
2019-09-19 20:23:13 -0500

    Modify the edit photo interface

--- Edit.Designer.cs
+++ Edit.Designer.cs
@@ -28,98 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.invertButton = new System.Windows.Forms.Button();
-            this.colorButton = new System.Windows.Forms.Button();
-            this.brightnessBar = new System.Windows.Forms.TrackBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(599, 374);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // invertButton
-            // 
-            this.invertButton.Location = new System.Drawing.Point(518, 399);
-            this.invertButton.Name = "invertButton";
-            this.invertButton.Size = new System.Drawing.Size(80, 32);
-            this.invertButton.TabIndex = 1;
-            this.invertButton.Text = "Invert";
-            this.invertButton.UseVisualStyleBackColor = true;
-            // 
-            // colorButton
-            // 
-            this.colorButton.Location = new System.Drawing.Point(291, 400);
-            this.colorButton.Name = "colorButton";
-            this.colorButton.Size = new System.Drawing.Size(81, 30);
-            this.colorButton.TabIndex = 2;
-            this.colorButton.Text = "Color...";
-            this.colorButton.UseVisualStyleBackColor = true;
-            // 
-            // brightnessBar
-            // 
-            this.brightnessBar.AutoSize = false;
-            this.brightnessBar.Location = new System.Drawing.Point(58, 416);
-            this.brightnessBar.Name = "brightnessBar";
-            this.brightnessBar.Size = new System.Drawing.Size(104, 56);
-            this.brightnessBar.TabIndex = 3;
-            this.brightnessBar.TickStyle = System.Windows.Forms.TickStyle.None;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 396);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Light";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(157, 396);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Dark";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(70, 455);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Brightness";
-            // 
-            // Edit
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 498);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.brightnessBar);
-            this.Controls.Add(this.colorButton);
-            this.Controls.Add(this.invertButton);
-            this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "Edit";
-            this.Text = "Edit Photo";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.invertButton = new System.Windows.Forms.Button();
+			this.colorButton = new System.Windows.Forms.Button();
+			this.brightnessBar = new System.Windows.Forms.TrackBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.cancelButton = new System.Windows.Forms.Button();
+			this.saveButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(449, 304);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// invertButton
+			// 
+			this.invertButton.Location = new System.Drawing.Point(369, 35);
+			this.invertButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.invertButton.Name = "invertButton";
+			this.invertButton.Size = new System.Drawing.Size(60, 26);
+			this.invertButton.TabIndex = 1;
+			this.invertButton.Text = "Invert";
+			this.invertButton.UseVisualStyleBackColor = true;
+			// 
+			// colorButton
+			// 
+			this.colorButton.Location = new System.Drawing.Point(199, 36);
+			this.colorButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.colorButton.Name = "colorButton";
+			this.colorButton.Size = new System.Drawing.Size(61, 24);
+			this.colorButton.TabIndex = 2;
+			this.colorButton.Text = "Color...";
+			this.colorButton.UseVisualStyleBackColor = true;
+			// 
+			// brightnessBar
+			// 
+			this.brightnessBar.AutoSize = false;
+			this.brightnessBar.Location = new System.Drawing.Point(20, 36);
+			this.brightnessBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.brightnessBar.Name = "brightnessBar";
+			this.brightnessBar.Size = new System.Drawing.Size(91, 36);
+			this.brightnessBar.TabIndex = 3;
+			this.brightnessBar.Tag = "";
+			this.brightnessBar.TickStyle = System.Windows.Forms.TickStyle.None;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(4, 21);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(30, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Light";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(98, 21);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(30, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Dark";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(36, 59);
+			this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(56, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Brightness";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.invertButton);
+			this.groupBox1.Controls.Add(this.brightnessBar);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.colorButton);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(10, 319);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(447, 83);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			// 
+			// cancelButton
+			// 
+			this.cancelButton.Location = new System.Drawing.Point(382, 412);
+			this.cancelButton.Name = "cancelButton";
+			this.cancelButton.Size = new System.Drawing.Size(75, 23);
+			this.cancelButton.TabIndex = 8;
+			this.cancelButton.Text = "Cancel";
+			this.cancelButton.UseVisualStyleBackColor = true;
+			// 
+			// saveButton
+			// 
+			this.saveButton.Location = new System.Drawing.Point(301, 412);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(75, 23);
+			this.saveButton.TabIndex = 9;
+			this.saveButton.Text = "Save";
+			this.saveButton.UseVisualStyleBackColor = true;
+			// 
+			// Edit
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(466, 447);
+			this.Controls.Add(this.saveButton);
+			this.Controls.Add(this.cancelButton);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Edit";
+			this.Text = "Edit Photo";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
 
         }
 
@@ -132,5 +175,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-    }
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button cancelButton;
+		private System.Windows.Forms.Button saveButton;
+	}
 }
