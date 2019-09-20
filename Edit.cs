6e284515d08a0c4cd61f5ab87be4c2265ad436e4
Daystar1998 <daystar.1998@yahoo.com>
2019-09-20 15:02:04 -0500

    Take in the image and fileName

--- Edit.cs
+++ Edit.cs
@@ -7,15 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PhotoEditor {
 
 	// TODO: Anchor the picture box to all sides after inserting an image to see if it scales correctly without going behind or in front of other items
 	public partial class Edit : Form {
 
-		public Edit() {
+		private Image image;
+		private string fileName;
+
+		public Edit(Image image, string fileName) {
 
 			InitializeComponent();
+
+			if(image == null) {
+
+				throw new ArgumentNullException(nameof(image));
+			}
+
+			if (fileName == null) {
+
+				throw new ArgumentNullException(nameof(fileName));
+			}
+
+			this.image = (Image)image.Clone();
+			pictureBox1.Image = this.image;
+			this.fileName = fileName;
 		}
 
 		private void CancelButton_Click(object sender, EventArgs e) {
