d8cb58abfb24812386d244e1ca0e9588e04c1931
Matthew Day <Daystar.1998@yahoo.com>
2019-09-21 02:38:55 -0500

    Implement the cancel button and expose the progress bar's value

--- Transforming.Designer.cs
+++ Transforming.Designer.cs
@@ -28,18 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.progressBar = new System.Windows.Forms.ProgressBar();
 			this.label1 = new System.Windows.Forms.Label();
 			this.cancelButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// progressBar1
+			// progressBar
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(11, 72);
-			this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(284, 37);
-			this.progressBar1.TabIndex = 0;
+			this.progressBar.Location = new System.Drawing.Point(11, 72);
+			this.progressBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(284, 37);
+			this.progressBar.TabIndex = 0;
 			// 
 			// label1
 			// 
@@ -52,22 +52,28 @@
 			// 
 			// cancelButton
 			// 
+			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.cancelButton.Location = new System.Drawing.Point(117, 127);
 			this.cancelButton.Name = "cancelButton";
 			this.cancelButton.Size = new System.Drawing.Size(75, 23);
 			this.cancelButton.TabIndex = 2;
 			this.cancelButton.Text = "Cancel";
 			this.cancelButton.UseVisualStyleBackColor = true;
+			this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
 			// 
 			// Transforming
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(306, 162);
+			this.ControlBox = false;
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.progressBar);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "Transforming";
 			this.Text = "Transforming";
 			this.ResumeLayout(false);
@@ -77,7 +83,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button cancelButton;
 	}
--- Transforming.cs
+++ Transforming.cs
@@ -8,13 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PhotoEditor
-{
-    public partial class Transforming : Form
-    {
-        public Transforming()
-        {
-            InitializeComponent();
-        }
-    }
+namespace PhotoEditor {
+
+	public partial class Transforming : Form {
+
+		public delegate void Cancel();
+		public event Cancel OnCancel;
+
+		public int ProgressPercentage {
+
+			set {
+
+				progressBar.Value = value;
+			}
+		}
+
+		public Transforming() {
+
+			InitializeComponent();
+		}
+
+		private void CancelButton_Click(object sender, EventArgs e) {
+
+			OnCancel();
+			this.Close();
+		}
+	}
 }
